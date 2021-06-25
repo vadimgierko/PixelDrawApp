84941ea6c670023127fc3a7143090b23188b331f
vadimgierko <85487884+vadimgierko@users.noreply.github.com>
2021-06-25 09:46:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,39 @@
+const style = {
+  backgroundColor: "blue",
+  width: "20px",
+  height: "20px",
+  border: "1px solid white"
+};
+
+function Pixel(props) {
+  return (
+    <td style={style}></td>
+  );
+}
+
+class Canvas extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      
+    }
+  }
+  render() {
+    return (
+      <div className="canvas">
+        <Pixel />
+        <Pixel />
+        <Pixel />
+      </div>
+    );
+  }
+}
+
 class App extends React.Component {
   render() {
     return (
       <div className="canvas">
-        <p>Hello</p>
+        <Canvas />
       </div>
     );
   }
