331bdd2e3f50f39b1aa8c0dd545fcadc63c906fa
vadimgierko <85487884+vadimgierko@users.noreply.github.com>
2021-06-25 09:09:50 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+class App extends React.Component {
+  render() {
+    return (
+      <div className="canvas">
+        <p>Hello</p>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById("root")
+);
